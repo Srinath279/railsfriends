3f4663488fe609ae8198aedc7d035527ad3f2ca1
Srinath279 <srinath12519@gmail.com>
2022-02-08 12:10:33 +0530

    initial RoR friends

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
+  resources :friends
+  # get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # root 'home#index'
+  root 'friends#index'
+  get 'home/about'
+end
